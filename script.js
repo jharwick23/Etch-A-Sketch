a3f8a2dbf3e50ab4f149041ff715b4b0bbd60a52
jharwick23 <146146809+jharwick23@users.noreply.github.com>
2024-11-05 18:53:49 -0500

    Finished script to change background color, create grid with button, and reset the grid.

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 const container = document.querySelector("#container");
+const button = document.querySelector(".button");
+let grid_size = 16;
 
-for(let i = 0; i < 64; i++){
+function createGrid(grid_size){
+    container.innerHTML = "";
 
-    let row = document.createElement("div");
-    container.appendChild(row);
-    row.classList.add("row-grid");
+    for(let i = 0; i < grid_size; i++){
 
-    for(let i = 0; i < 64; i++){
+        let row = document.createElement("div");
+        container.appendChild(row);
+        row.classList.add("row-grid");
 
-        let column = document.createElement("div");
-        row.appendChild(column); 
-        column.classList.add("column-grid")
-
+        for(let j = 0; j < grid_size; j++){
+        
+            let column = document.createElement("div");
+            row.appendChild(column); 
+            column.classList.add("column-grid")
+        
+        }
     }
+    const columnGrid = document.querySelectorAll(".column-grid");
+
+    columnGrid.forEach(columns => {
+        columns.addEventListener("mouseenter", () => {
+            columns.style.backgroundColor = "blue";
+        });
+    });
 }
+
+createGrid(grid_size);
+
+const btn = document.querySelector("#btn")
+btn.addEventListener("click", () => {
+
+    while (true) {
+        let input = prompt("Please enter grid size:");
+        grid_size = parseInt(input);
+
+        if (isNaN(grid_size) || grid_size < 0 || grid_size > 100){
+            alert("PLEASE ENTER VALID NUMBER 0-100!")
+        }
+        else{
+            break;
+        }
+    }
+
+    createGrid(grid_size);
+});
